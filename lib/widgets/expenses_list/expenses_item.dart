8395046ac70bb27c2f67d0e24ad5c43ed94b1571
joseph <josephchome888@gmail.com>
2025-05-17 18:35:05 +0900

    [feat] 파일 구조 변경경

---
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -0,0 +1,38 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesItem extends StatelessWidget {
+  final Expense expense;
+
+  const ExpensesItem(this.expense, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0),
+        child: Column(
+          children: <Widget>[
+            Text(expense.title),
+            const SizedBox(height: 4),
+            Row(
+              children: <Widget>[
+                Text(
+                  '\$${expense.amount.toStringAsFixed(2)}', // 두 자리 수까지 표현 12.3433 => 12.34
+                ),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.alarm),
+                    const SizedBox(width: 8,),
+                    Text(expense.date.toString())
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
