975b9f4ac13ae8dc97fe5a7be3b0d2101fb5b98d
joseph <josephchome888@gmail.com>
2025-05-18 15:46:05 +0900

    [feat] Expense 모델 개선 1. category 맵 데이터 2. formattedDate

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,21 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
+
+// Category 키를 값으로 갖고, 키에 매칭되는 아이콘을 value로 갖는 Map 리턴
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
 
 class Expense {
   final String id;
@@ -11,6 +24,14 @@ class Expense {
   final DateTime date;
   final Category category;
 
-  Expense({required this.title,required this.amount, required this.date, required this.category})
-    : id = uuid.v4();
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
